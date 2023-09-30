60a41f4095776ca1bdfd1993059dcccc127158e2
SpringFlutter <109391178+SpringFlutter@users.noreply.github.com>
2023-09-30 12:07:27 -0500

    Update prueba2.py

--- prueba2.py
+++ prueba2.py
@@ -2,14 +2,19 @@ from flask import Flask, render_template
 from flask_pymongo import PyMongo
 
 app = Flask(__name__, template_folder='')
+
+# Conexion a la base de datos dbDatos en la nube
 app.config['MONGO_URI'] = 'mongodb+srv://projectspring85:prueba123@cluster0.xqrsb1a.mongodb.net/dbDatos?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
 
+# Definir API GET para consumir los datos, recibira el parametro idInstitucion
 @app.route('/api/datos/institucion/<string:idInstitucion>', methods=['GET'])
 def get_data(idInstitucion):
     data = list(mongo.db.docInstitucion.find({'institucion_id':idInstitucion}, {'_id': 0}))
+    # Se imprime los datos
     print(data)
+    # Se regresa los datos en una pagina.html
     if data:
         return render_template('pagina.html', data=data)
     else:
